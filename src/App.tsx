fcdb902dda320546f0b4c56fe5d9fa4b365ef148
Gergo Buglyo <buglyogergo@gmail.com>
2023-02-28 23:34:56 +0100

    Created inputfield state

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,17 @@
+import React, { useState } from "react";
+
 import "./App.css";
 import InputField from "./components/InputField";
 
 const App: React.FC = () => {
+  const [todo, setTodo] = useState<string>("");
+
+  console.log(todo);
+
   return (
     <div className="App">
       <h1>To-do</h1>
-      <InputField />
+      <InputField setTodo={setTodo} />
     </div>
   );
 };
--- src/components/InputField.tsx
+++ src/components/InputField.tsx
@@ -1,13 +1,29 @@
-import React from "react";
+import React, { useRef } from "react";
 import "./InputField.css";
 
-const InputField = () => {
+interface Props {
+  setTodo: React.Dispatch<React.SetStateAction<string>>;
+}
+
+const InputField = ({ setTodo }: Props) => {
+  const inputRef = useRef<HTMLInputElement>(null);
+
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>): void => {
+    event.preventDefault();
+    const inputValue = inputRef.current?.value;
+    setTodo(inputValue!);
+    if (inputRef.current) {
+      inputRef.current.value = "";
+    }
+  };
+
   return (
-    <form className="form">
+    <form className="form" onSubmit={handleSubmit}>
       <input
         type="input"
         className="form-input"
         placeholder="Enter a task..."
+        ref={inputRef}
       />
       <input type="submit" className="form-btn" value="go" />
     </form>
